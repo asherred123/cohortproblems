c714a580107a1a54f44413693f0f6044ef5cabf6
asherred123 <99535490+asherred123@users.noreply.github.com>
2024-06-28 16:28:59 +0800

    yes

--- ce5/ce5_q1/routes/dept.js
+++ ce5/ce5_q1/routes/dept.js
@@ -5,22 +5,46 @@ var router = express.Router();
 
 
 router.get('/add/:code', async function(req, res, next) {
-    
-    res.send(`TODO`); // TODO: Fixme
+    try {
+        const code = req.params.code;
+        const result = await deptCollection.insertOne({ code: code });
+        res.status(201).send({ "code": code });
+    } catch (error) {
+        next(error);
+    }
 });
 
 
 
 /* GET dept listing. */
 
-router.get('/all/', async function(req, res, next) {
-    res.send(`TODO`); // TODO: Fixme
+router.get('/all', async (req, res, next) => {
+    try {
+        const departments = await deptCollection.find({}).toArray();
+        res.status(200).json(departments);
+    } catch (error) {
+        next(error);
+    }
 });
 
 
-router.get('/all/withstaff/', async function(req, res, next) {
-    res.send(`TODO`); // TODO: Fixme
-})
+router.get('/all/withstaff', async (req, res, next) => {
+    try {
+        const departments = await deptCollection.aggregate([
+            {
+                $lookup: {
+                    from: 'staff',
+                    localField: '_id',
+                    foreignField: 'deptId',
+                    as: 'staff'
+                }
+            }
+        ]).toArray();
+        res.status(200).json(departments);
+    } catch (error) {
+        next(error);
+    }
+});
 
 
 module.exports = router;
